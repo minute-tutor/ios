499ceeba762635443cfec4fbea65be72b473ea6f
psonthalia <paran.sonthalia@gmail.com>
2016-06-11 22:06:18 -0700

    Finished My Students Page

---
+++ Minute Tutor/MyStudents.h
@@ -0,0 +1,15 @@
+//
+//  MyStudents.h
+//  Minute Tutor
+//
+//  Created by Paran Sonthalia on 6/11/16.
+//  Copyright © 2016 Mango Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyStudents : UIViewController <UITableViewDelegate, UITableViewDataSource> {
+    NSArray *tableData;
+}
+
+@end
---
+++ Minute Tutor/MyStudents.m
@@ -0,0 +1,47 @@
+//
+//  MyStudents.m
+//  Minute Tutor
+//
+//  Created by Paran Sonthalia on 6/11/16.
+//  Copyright © 2016 Mango Apps. All rights reserved.
+//
+
+#import "MyStudents.h"
+
+@interface MyStudents ()
+
+@end
+
+@implementation MyStudents
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    tableData = [NSArray arrayWithObjects:@"Date - Name - Time", @"Date - Name - Time", @"Date - Name - Time", @"Date - Name - Time", @"Date - Name - Time", @"Date - Name - Time", @"Date - Name - Time", @"Date - Name - Time", @"Date - Name - Time", nil];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [tableData count];
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    return cell;
+}
+
+@end
